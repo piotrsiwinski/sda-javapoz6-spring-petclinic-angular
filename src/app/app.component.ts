d849482aac901f9b3881c52ed1b29ed3999647ba
Weronika Witek <incomia.pl@gmail.com>
2018-07-15 15:50:04 +0200

    ui logo - modifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from './auth/authentication.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+ // title = 'app';
+  isLogged;
+  constructor (private auth: AuthenticationService) {
+  }
+
+  ngOnInit() {
+    this.isLogged = this.auth.isUserLogged();
+  }
 }
