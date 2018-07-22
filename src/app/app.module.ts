57dea1c66d450e252426ddda1b963cc71f8e0a9f
Jakub <jakub.mitulski@gmail.com>
2018-07-22 11:44:13 +0200

    Added authientication guard for path /owners

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import {HttpClientModule} from '@angular/common/http';
 import { LoginComponent } from './auth/login/login.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { RegisterComponent } from './auth/register/register.component';
+import {AuthService} from './auth/auth.service';
+import {AuthGuard} from './auth/auth.guard';
 
 @NgModule({
   declarations: [
@@ -32,7 +34,7 @@ import { RegisterComponent } from './auth/register/register.component';
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [OwnerService],
+  providers: [OwnerService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
