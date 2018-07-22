d9dce665f696b9729e128eb96ed8e728570904c4
Piotr Siwiński <piotr.siwinski@f-secure.com>
2018-07-22 13:15:00 +0200

    Change service to prod

--- src/app/auth/login/login.service.ts
+++ src/app/auth/login/login.service.ts
@@ -1,7 +1,7 @@
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {CookieService} from 'ngx-cookie-service';
-import {environment} from '../../../environments/environment.prod';
+import {environment} from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
